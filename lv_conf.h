7c912865eb45468d011ec2bb0813bdf148bdd0fe
Mike Schneider <michschn@gmail.com>
2025-02-26 23:02:09 +0100

    enable png decoding

--- lv_conf.h
+++ lv_conf.h
@@ -742,7 +742,7 @@
 /*API for LittleFs. */
 #define LV_USE_FS_LITTLEFS 0
 #if LV_USE_FS_LITTLEFS
-    #define LV_FS_LITTLEFS_LETTER '\0'     /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
+    #define LV_FS_LITTLEFS_LETTER 'A'     /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
 #endif
 
 /*API for Arduino LittleFs. */
@@ -758,7 +758,7 @@
 #endif
 
 /*LODEPNG decoder library*/
-#define LV_USE_LODEPNG 0
+#define LV_USE_LODEPNG 1
 
 /*PNG decoder(libpng) library*/
 #define LV_USE_LIBPNG 0
